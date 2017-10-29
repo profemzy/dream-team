515e37e84531879cdd19b7d21cbcc8d905ca3c2b
Oladele Femi <profemzy@gmail.com>
2017-10-29 19:10:00 +0100

    Added Production Deployment Scripts

---
+++ install_script.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#!/bin/bash
+
+# install web server dependencies
+sudo apt-get update
+sudo apt-get -y install python python-virtualenv nginx supervisor
+
+# install application (source location in $1)
+mkdir /home/profemzy/python-ibadan
+cp -R $1/python-ibadan/* /home/profemzy/python-ibadan/
+
+# create a virtualenv and install dependencies
+virtualenv /home/profemzy/python-ibadan/venv
+/home/profemzy/python-ibadan/venv/bin/pip install -r /home/profemzy/python-ibadan/requirements.txt
+
+# configure supervisor
+sudo cp /profemzy/python-ibadan.conf /etc/supervisor/conf.d/
+sudo mkdir /var/log/python-ibadan
+sudo supervisorctl reread
+sudo supervisorctl update
+
+# configure nginx
+sudo cp /profemzy/python-ibadan.nginx /etc/nginx/sites-available/python-ibadan
+sudo rm -f /etc/nginx/sites-enabled/default
+sudo ln -s /etc/nginx/sites-available/python-ibadan /etc/nginx/sites-enabled/
+sudo service nginx restart
+
+echo Application deployed to http://40.68.88.198/
